f04a9742158659616f17566d25cf2a998cbe2ca5
CypherTech16 <patelkushal1877@gmail.com>
2026-02-16 13:49:49 -0500

    uncomment UI file from index file

--- app/screens/index.tsx
+++ app/screens/index.tsx
@@ -1,7 +1,7 @@
 import { View, Button, FlatList, StyleSheet, Text } from "react-native";
 import { useRouter } from "expo-router";
 import { getWins } from "../../data/winsData";
-//import WinItem from "../../components/WinItem";
+import WinItem from "../../components/WinItem";
 import { useEffect, useState } from "react";
 
 export default function HomeScreen() {
